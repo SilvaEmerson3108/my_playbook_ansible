15fb7e8159251aab3c927da2f10ebf7aebe32921
SilvaEmerson3108 <ricardosilva.emerson@gmail.com>
2021-08-11 19:57:44 -0300

    diretorio

---
+++ install_zabbix_agent/create-host-zabbix.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# 1. set connection details
+url=https://zabbix.qa.dpsp.io/api_jsonrpc.php
+user=Admin
+password='zabbix'
+
+# 2. get authorization token
+auth=$(curl -s -X POST \
+-H 'Content-Type: application/json-rpc' \
+-d " \
+{
+ \"jsonrpc\": \"2.0\",
+ \"method\": \"user.login\",
+ \"params\": {
+  \"user\": \"$user\",
+  \"password\": \"$password\"
+ },
+ \"id\": 1,
+ \"auth\": null
+}
+" $url | \
+jq -r '.result'
+)
+
+curl -s -X POST \
+-H 'Content-Type: application/json-rpc' \
+-d " \
+{
+    \"jsonrpc\": \"2.0\",
+    \"method\": \"host.create\",
+    \"params\": {
+        \"host\": \"insira_o_hostname\",
+        \"interfaces\": [
+            {
+                \"type\": 1,
+                \"main\": 1,
+                \"useip\": 1,
+                \"ip\": \"insira_o_ip\",
+                \"dns\": \"\",
+                \"port\": \"10051\"
+            }
+        ],
+        \"groups\": [
+            {
+                \"groupid\": \"2\"
+            }
+        ],
+        \"templates\": [
+            {
+                \"templateid\": \"10001\"
+            }
+        ]   
+    },
+    \"auth\": \"$auth\",
+    \"id\": 3
+} 
+" $url 
